bef5af934789f55a440aa7da8747dbc3eb74491e
U-DESKTOP-HL2MJ28\Aniban <anirbanaditya05@gmail.com>
2021-12-13 16:45:34 +0530

    First Commit

---
+++ src/main/java/com/techcloudmicro/bigbucketdemo/BigbucketdemoApplication.java
@@ -0,0 +1,13 @@
+package com.techcloudmicro.bigbucketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbucketdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbucketdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcloudmicro/bigbucketdemo/BigbucketdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcloudmicro.bigbucketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigbucketdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
